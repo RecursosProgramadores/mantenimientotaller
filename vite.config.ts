d11e90adbc2fb38f9c5c2d758fa362d4e094dcbb
jheremy99 <999935626g@gmail.com>
2026-07-08 17:16:01 -0500

    soluion previa

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/mantenimientotaller/",
   plugins: [
     TanStackRouterVite({
       routesDirectory: "./src/routes",
